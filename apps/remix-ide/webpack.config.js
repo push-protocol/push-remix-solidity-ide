96ef1376372e73b6635d605d6c84badf4e489888
corlard3y <corlardey@gmail.com>
2025-06-09 21:18:31 +0100

    update basehref and add gh-pages workflow

--- apps/remix-ide/webpack.config.js
+++ apps/remix-ide/webpack.config.js
@@ -100,9 +100,9 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
 
   // add public path
   if(process.env.NX_DESKTOP_FROM_DIST){
-    config.output.publicPath = './'
+    config.output.publicPath = '/push-remix-solidity-ide/'
   }else{
-    config.output.publicPath = '/'
+    config.output.publicPath = '/push-remix-solidity-ide/'
   }
 
   // set filename
